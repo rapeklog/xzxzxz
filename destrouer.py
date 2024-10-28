bcd831562fcf9d04acb958d43b2b59a62ec617c8
rapeklog <167132424+rapeklog@users.noreply.github.com>
2024-10-29 01:18:11 +0300

    Add files via upload

---
+++ destrouer.py
@@ -0,0 +1,44 @@
+import sys,os
+from colorama import Fore
+
+print(Fore.MAGENTA+"""
+█████████████████████████████████████████████████████████████████████████████
+
+▀▀█▀▀ ▒█░▒█ ▒█▀▀▀ 　 ▒█▀▀▄ ▒█▀▀▀ ▒█▀▀▀█ ▀▀█▀▀ ▒█▀▀█ ▒█▀▀▀█ ▒█░░▒█ ▒█▀▀▀ ▒█▀▀█ 
+░▒█░░ ▒█▀▀█ ▒█▀▀▀ 　 ▒█░▒█ ▒█▀▀▀ ░▀▀▀▄▄ ░▒█░░ ▒█▄▄▀ ▒█░░▒█ ▒█▄▄▄█ ▒█▀▀▀ ▒█▄▄▀ 
+░▒█░░ ▒█░▒█ ▒█▄▄▄ 　 ▒█▄▄▀ ▒█▄▄▄ ▒█▄▄▄█ ░▒█░░ ▒█░▒█ ▒█▄▄▄█ ░░▒█░░ ▒█▄▄▄ ▒█░▒█
+      
+█████████████████████████████████████████████████████████████████████████████
+                                                                 
+слито в @pr0xit
+
+BY: Zero Offens Security 
+""")
+
+def display_menu():
+    print(Fore.GREEN + """
+    1: THE DESTROYER (Hacking Tools)      | 2: Zero-Paid-Tools
+    3: Info (about-us)
+    """)
+
+def execute_command(command):
+    if command == '1':
+        os.system('cmd /k "python Zero-Tool/zero-tool.py"' if os.name == 'nt' else 'python Zero-Tool/zero-tool.py')
+    elif command == '2':
+        print(Fore.RED + 'This option is not available yet! Coming soon...')
+        #os.system('cmd /k "python Zero-Web-Hacktool/web_bugger.py"')
+    elif command == '3':
+        os.system('cmd /k "python info.py"' if os.name == 'nt' else 'python info.py')
+
+        display_menu()
+    else:
+        print('Invalid option! Please choose the correct one.')
+
+while True:
+    display_menu()
+    command = input('> ')
+
+    if command.lower() == 'exit':
+        break
+
+    execute_command(command)
